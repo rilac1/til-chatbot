c8a607cadfb18dd0cdacd8b752fdb1687772ffee
kang <rilac1@naver.com>
2022-05-22 19:07:20 +0900

    :sparkles: RecordMemoryService 조회 기능 추가

--- src/main/java/com/dku/tilchatbot/record/service/RecordManageService.java
+++ src/main/java/com/dku/tilchatbot/record/service/RecordManageService.java
@@ -1,9 +1,16 @@
 package com.dku.tilchatbot.record.service;
 
+import com.dku.tilchatbot.record.model.ActivityCategory;
 import com.dku.tilchatbot.record.model.Record;
 import com.dku.tilchatbot.record.repository.RecordRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+
+import java.time.Duration;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.OptionalDouble;
+import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
@@ -18,5 +25,33 @@ public class RecordManageService implements RecordService {
         }
         final var savedRecord = recordRepository.save(record);
         System.out.println(savedRecord);
+    }
+
+    @Override
+    public List<Record> lookUpRecord(ActivityCategory activityCategory) {
+        return recordRepository.find(activityCategory);
+    }
+
+    @Override
+    public List<Record> lookupRecord(ActivityCategory activityCategory, LocalDate lookupDate) {
+        return recordRepository.find(activityCategory, lookupDate);
+    }
+
+    @Override
+    public long totalTime(List<Record> recordList) {
+        return recordList.stream()
+                .map(record -> Duration.between(record.getStartTime(), record.getEndTime()).toMinutes())
+                .mapToLong(i->i)
+                .sum();
+    }
+
+    @Override
+    public long averageTime(List<Record> recordList) {
+        OptionalDouble average =  recordList.stream()
+                .map(record -> Duration.between(record.getStartTime(), record.getEndTime()).toMinutes())
+                .mapToLong(i->i)
+                .average();
+
+        return Double.valueOf(String.valueOf(average)).longValue();
     }
 }
--- src/main/java/com/dku/tilchatbot/record/service/RecordService.java
+++ src/main/java/com/dku/tilchatbot/record/service/RecordService.java
@@ -1,7 +1,17 @@
 package com.dku.tilchatbot.record.service;
 
+import com.dku.tilchatbot.record.model.ActivityCategory;
 import com.dku.tilchatbot.record.model.Record;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface RecordService {
     void saveRecord(Record record);
+
+    List<Record> lookUpRecord(ActivityCategory activityCategory);
+    List<Record> lookupRecord(ActivityCategory activityCategory, LocalDate lookupDate);
+
+    long totalTime(List<Record> recordList);
+    long averageTime(List<Record> recordList);
 }
