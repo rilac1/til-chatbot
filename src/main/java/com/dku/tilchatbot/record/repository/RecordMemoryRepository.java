6e2ab478ccc9e1b49da519d5abf25330e6baa9f9
kang <rilac1@naver.com>
2022-05-22 19:06:51 +0900

    :sparkles: RecordMemoryRepository 조회 기능 추가

--- src/main/java/com/dku/tilchatbot/record/repository/RecordMemoryRepository.java
+++ src/main/java/com/dku/tilchatbot/record/repository/RecordMemoryRepository.java
@@ -1,10 +1,14 @@
 package com.dku.tilchatbot.record.repository;
 
+import com.dku.tilchatbot.record.model.ActivityCategory;
 import com.dku.tilchatbot.record.model.Record;
 import org.springframework.stereotype.Repository;
 
+import java.time.LocalDate;
 import java.util.HashMap;
+import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Repository
 public class RecordMemoryRepository implements RecordRepository {
@@ -21,5 +25,21 @@ public class RecordMemoryRepository implements RecordRepository {
         record.setId(incrementIndex++);
         recordMap.put(record.getId(), record);
         return record;
+    }
+
+    @Override
+    public List<Record> find(ActivityCategory activityCategory) {
+        return recordMap.values().stream()
+                .filter(record -> record.getActivityCategory()
+                        .equals(activityCategory))
+                .collect(Collectors.toList());
+    }
+
+    @Override
+    public List<Record> find(ActivityCategory activityCategory, LocalDate lookupDate) {
+        return find(activityCategory).stream()
+                .filter(record -> record.getStartTime().toLocalDate()
+                        .equals(lookupDate))
+                .collect(Collectors.toList());
     }
 }
--- src/main/java/com/dku/tilchatbot/record/repository/RecordRepository.java
+++ src/main/java/com/dku/tilchatbot/record/repository/RecordRepository.java
@@ -1,7 +1,13 @@
 package com.dku.tilchatbot.record.repository;
 
+import com.dku.tilchatbot.record.model.ActivityCategory;
 import com.dku.tilchatbot.record.model.Record;
+
+import java.time.LocalDate;
+import java.util.List;
 
 public interface RecordRepository {
     Record save(Record record);
+    List<Record> find(ActivityCategory activityCategory);
+    List<Record> find(ActivityCategory activityCategory, LocalDate lookupDate);
 }
